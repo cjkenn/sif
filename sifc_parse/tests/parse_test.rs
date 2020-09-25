4d99afd44ea9204d45a3598b8ea6ee1e142f07cf
cjkenn <cjkenn@users.noreply.github.com>
2020-09-25 11:26:42 -0400

    move tests

---
+++ sifc_parse/tests/parse_test.rs
@@ -0,0 +1,161 @@
+use sifc_parse::{lex::Lexer, parser::Parser, symtab::SymTab};
+
+use std::fs::File;
+
+mod util;
+
+#[test]
+fn if_stmt() {
+    let pctx = util::setup("if_stmt");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn for_stmt() {
+    let pctx = util::setup("for_stmt");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn var_decl_valid() {
+    let pctx = util::setup("var_decl");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn var_decl_invalid() {
+    let pctx = util::setup("var_decl_invalid");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn fn_decl_valid() {
+    let pctx = util::setup("fn_decl_valid");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn fn_decl_invalid() {
+    let pctx = util::setup("fn_decl_invalid");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn fn_w_ret_stmt() {
+    let pctx = util::setup("fn_w_ret_stmt");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn fn_call() {
+    let pctx = util::setup("fn_call");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn exprs() {
+    let pctx = util::setup("exprs");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn table_decl() {
+    let pctx = util::setup("table_decl");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn record_decl() {
+    let pctx = util::setup("record_decl");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
+
+#[test]
+fn array_decl() {
+    let pctx = util::setup("array_decl");
+
+    let infile = File::open(&pctx.path).unwrap();
+    let mut symtab = SymTab::new();
+    let mut lex = Lexer::new(infile);
+    let mut parser = Parser::new(&mut lex, &mut symtab);
+
+    let result = parser.parse();
+    util::check(pctx, result);
+}
