39d5d93787c3748ebe0527cae408404227115748
cjkenn <cjkenn@users.noreply.github.com>
2020-10-22 13:35:33 -0400

    greatly simplify parser tests

--- sifc_parse/tests/README.md
+++ sifc_parse/tests/README.md
@@ -5,23 +5,17 @@ These tests can be run using `cargo test` from the sif root directory (or from a
 The tests work by reading input files from the `input` directory. These are `.sif` files, and named
 according to the functionality they test (ie. `for_stmt.sif` tests a for loop), but there is no official
 naming convention as of now. Each file has a comment at the top detailing the expectations of the test:
-whether the parser should pass or fail, and if it fails, where it is supposed to fail. The syntax of the
-expectations string is:
+whether the parser should pass or fail. The syntax of the expectations string is:
 
 ```
-# expect::pass | fail::line::pos
+# expect::pass | fail
 ```
 
-Expectation parsing is done in [util/mod.rs](https://github.com/cjkenn/sif/blob/master/tests/util/mod.rs#L75).
-
-The [setup](https://github.com/cjkenn/sif/blob/master/tests/util/mod.rs#L16) method opens the input file at
-the path provided, parses the expectations and returns a `ParseTestCtx` struct to set up the test. To test
-a file, create a new `#[test]` method, call `util::setup(...)`, read the file, and then call `util::check`
-to determine if the test passed. Check will see that the expectations matched, and if the test failed at the desired point in the file.
+The `setiup` method opens the input file at the path provided, parses the expectations and returns
+a `ParseTestCtx` struct to set up the test.
 
 ## Adding a new test
 
 1. Create a new sif file in the `input` directory. Ensure the parse expectations are defined on the first line.
-2. Add the test name to the paths map in the `get_test_path` function.
-3. Create the test in `parse_test.rs`. Ensure that the string passed to `util::setup`
-is the same key that was added to the paths map in step 2.
+2. Use the `test_parse` macro, passing in your test name and then the filename as a `&str`. The macro will
+create the test.
--- sifc_parse/tests/parse_test.rs
+++ sifc_parse/tests/parse_test.rs
@@ -1,187 +1,111 @@
-use sifc_parse::{lex::Lexer, parser::Parser, symtab::SymTab};
+use sifc_parse::{
+    lex::Lexer,
+    parser::{Parser, ParserResult},
+    symtab::SymTab,
+};
 
-use std::fs::File;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-mod util;
-
-#[test]
-fn if_stmt() {
-    let pctx = util::setup("if_stmt");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
+pub struct ParseTestCtx {
+    pub path: String,
+    pub is_pass: bool,
 }
 
-#[test]
-fn for_stmt() {
-    let pctx = util::setup("for_stmt");
+fn setup(test_name: &str) -> ParseTestCtx {
+    let path = format!("./tests/input/{}.sif", test_name);
+    let expfile = File::open(&path).unwrap();
+    let exp = BufReader::new(expfile).lines().next().unwrap().unwrap();
 
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
+    let pe = match parse_expectations(&exp, &path) {
+        Ok(p) => p,
+        Err(e) => panic!(e),
+    };
 
-    let result = parser.parse();
-    util::check(pctx, result);
+    pe
 }
 
-#[test]
-fn var_decl_valid() {
-    let pctx = util::setup("var_decl");
+fn parse_expectations(expectations: &str, test_path: &str) -> Result<ParseTestCtx, &'static str> {
+    let parts: Vec<&str> = expectations.split("::").collect();
 
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
+    if parts.len() == 0 {
+        return Err("No test expectations string found");
+    }
 
-    let result = parser.parse();
-    util::check(pctx, result);
+    if parts.len() < 2 {
+        return Err("Invalid test expectation string. Usage: 'expect::[pass][fail]'");
+    }
+
+    if !parts[0].contains("expect") {
+        return Err("Invalid test expectation string. Usage: 'expect::[pass][fail]'");
+    }
+
+    if parts[1] != "fail" && parts[1] != "pass" {
+        return Err("Invalid test expectation string. Usage: 'expect::[pass][fail]'");
+    }
+
+    if parts[1] == "pass" {
+        return Ok(ParseTestCtx {
+            path: String::from(test_path),
+            is_pass: true,
+        });
+    }
+
+    Ok(ParseTestCtx {
+        path: String::from(test_path),
+        is_pass: false,
+    })
 }
 
-#[test]
-fn var_decl_invalid() {
-    let pctx = util::setup("var_decl_invalid");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
+fn check(pctx: ParseTestCtx, result: ParserResult) {
+    match pctx.is_pass {
+        true => {
+            if result.has_err {
+                assert!(false, "FAIL: {:?} expected successful parse, found error");
+            } else {
+                assert!(true);
+            }
+        }
+        false => {
+            if !result.has_err {
+                assert!(false, "FAIL: {:?} expected error, found none",);
+            } else {
+                assert!(true);
+            }
+        }
+    }
 }
 
-#[test]
-fn fn_decl_valid() {
-    let pctx = util::setup("fn_decl_valid");
+macro_rules! test_parse {
+    ($test_name:ident, $file_name:expr) => {
+        #[test]
+        fn $test_name() {
+            let pctx = setup($file_name);
 
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
+            let infile = File::open(&pctx.path).unwrap();
+            let mut symtab = SymTab::new();
+            let mut lex = Lexer::new(infile);
+            let mut parser = Parser::new(&mut lex, &mut symtab);
 
-    let result = parser.parse();
-    util::check(pctx, result);
+            let result = parser.parse();
+            check(pctx, result);
+        }
+    };
 }
 
-#[test]
-fn fn_decl_invalid() {
-    let pctx = util::setup("fn_decl_invalid");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
-
-#[test]
-fn fn_decl_wrong_params() {
-    let pctx = util::setup("fn_decl_wrong_params");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
-
-#[test]
-fn fn_w_ret_stmt() {
-    let pctx = util::setup("fn_w_ret_stmt");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
-
-#[test]
-fn fn_call() {
-    let pctx = util::setup("fn_call");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
-
-#[test]
-fn fn_call_wrong_params() {
-    let pctx = util::setup("fn_call_wrong_params");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
-
-#[test]
-fn exprs() {
-    let pctx = util::setup("exprs");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
-
-#[test]
-fn table_decl() {
-    let pctx = util::setup("table_decl");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
-
-#[test]
-fn array_decl() {
-    let pctx = util::setup("array_decl");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
-
-#[test]
-fn table_access_invalid() {
-    let pctx = util::setup("table_access_invalid");
-
-    let infile = File::open(&pctx.path).unwrap();
-    let mut symtab = SymTab::new();
-    let mut lex = Lexer::new(infile);
-    let mut parser = Parser::new(&mut lex, &mut symtab);
-
-    let result = parser.parse();
-    util::check(pctx, result);
-}
+test_parse!(if_stmt, "if_stmt");
+test_parse!(for_stmt, "for_stmt");
+test_parse!(var_decl, "var_decl");
+test_parse!(var_decl_invalid, "var_decl_invalid");
+test_parse!(fn_decl_valid, "fn_decl_valid");
+test_parse!(fn_decl_invalid, "fn_decl_invalid");
+test_parse!(fn_decl_wrong_params, "fn_decl_wrong_params");
+test_parse!(fn_w_ret_stmt, "fn_w_ret_stmt");
+test_parse!(fn_call, "fn_call");
+test_parse!(fn_call_wrong_params, "fn_call_wrong_params");
+test_parse!(exprs, "exprs");
+test_parse!(table_decl, "table_decl");
+test_parse!(array_decl, "array_decl");
+test_parse!(table_access_invalid, "table_access_invalid");
